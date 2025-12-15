8491d526862ba026ff8c9bbe54c2deb59ea617a6
JJK3187 <truestory14@kakao.com>
2025-12-15 10:18:24 +0900

    커머스 과제 step1 Product 클래스를 생성하여 상품명, 가격, 설명, 재고수량으로 객체를 생성 반복문을 통해 상품리스트 구현

--- src/step1/Main.java
+++ src/step1/Main.java
@@ -1,4 +1,45 @@
 package step1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        List<Product> products = new ArrayList<>();
+        Scanner scanner =  new Scanner(System.in);
+
+        Product product1 = new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 10 );
+        Product product2 = new Product("iPhone 16",1350000,"Apple의 최신 스마트폰",20);
+        Product product3 = new Product("MacBook Pro",2400000,"M3 칩셋이 탑재된 노트북",5);
+        Product product4 = new Product("AirPods Pro",350000,"노이즈 캔슬링 무선 이어폰",40);
+        products.add(product1);
+        products.add(product2);
+        products.add(product3);
+        products.add(product4);
+
+        System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
+        for (Product product : products) {
+            System.out.println("1. " + product1.getName() + " | " + product1.getPrice() + " | " + product1.getDescriprtion());
+            System.out.println("2. " + product2.getName() + " | " + product2.getPrice() + " | " + product2.getDescriprtion());
+            System.out.println("3. " + product3.getName() + " | " + product3.getPrice() + " | " + product3.getDescriprtion());
+            System.out.println("4. " + product4.getName() + " | " + product4.getPrice() + " | " + product4.getDescriprtion());
+            System.out.println("0. 종료" + " | " + "프로그램 종료");
+            String exit = scanner.nextLine();
+           if (exit.equals("0")) {
+               break;
+           }
+        }
+        System.out.println("커머스 플랫폼을 종료합니다.");
+        scanner.close();
+
+
+
+
+
+
+
+    }
 }
--- src/step1/Product.java
+++ src/step1/Product.java
@@ -1,4 +1,34 @@
 package step1;
 
 public class Product {
+
+    private String name;
+    private int price;
+    private String descriprtion;
+    private int quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getDescriprtion() {
+        return descriprtion;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+
+    public Product(String name, int price, String descriprtion, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.descriprtion = descriprtion;
+        this.quantity = quantity;
+    }
+
 }
